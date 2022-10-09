a1955575ab036b4fd1430386d40f8a0f9e21c2c6
Michiru125 <114606491+Michiru125@users.noreply.github.com>
2022-10-09 23:01:12 +0200

    NEvsorban_novekvo_magassag

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Magassag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region első próbálkozás
+            /*
+            int N;
+            N = int.Parse(Console.ReadLine());
+
+            if (1<= N && N <= 100)
+            {
+                Console.WriteLine("jó lesz, jöhetnek az adatok");
+            }
+            else
+            {
+                Console.WriteLine("nem tetszik.");
+            }
+            */
+            #endregion
+
+            int N;
+            bool jo;
+            do
+            {
+                jo = int.TryParse(Console.ReadLine(),out N);
+                jo = jo && (1 <= N && N <= 100);
+            } while (!jo);
+            
+            int[] H = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+              string[] sor = Console.ReadLine().Split(' ');
+                Console.WriteLine(sor[0]);
+                H[i] = int.Parse(sor[1]);
+            }
+
+            
+
+            Console.WriteLine("Ez jó lesz: " + N);
+            Console.WriteLine("IGEN");
+        }
+    }
+}
